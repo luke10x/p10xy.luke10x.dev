42929904147c01de1051205a9881f94de978e9e4
CI Autobot <spam@luke10x.dev>
2020-11-02 16:38:26 +0000

    yet another release

---
+++ precache-manifest.5b544e992f1c175381f88dcd1e367f77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c446d0caaa5dfaf1c8ddd79df24bed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1ceaf1187907c93b323",
+    "url": "/static/css/main.9694fc4a.chunk.css"
+  },
+  {
+    "revision": "0df317e3c3d1ab29baba",
+    "url": "/static/js/2.b8f6695a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b8f6695a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1ceaf1187907c93b323",
+    "url": "/static/js/main.5ff61481.chunk.js"
+  },
+  {
+    "revision": "598326a144976ab7cd59",
+    "url": "/static/js/runtime-main.e7feca8e.js"
+  }
+]);
